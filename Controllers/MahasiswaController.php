4e557e1aec555f532bc71e56724d86cb6d582830
KhairaZahrani <khairazahrani50@gmail.com>
2024-08-09 11:35:22 +0700

    first commit

--- Controllers/MahasiswaController.php
+++ Controllers/MahasiswaController.php
@@ -3,54 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-// use App\Models\Mahasiswa;
+use Illuminate\Support\Facades\Auth;
 
 class MahasiswaController extends Controller
 {
-
-    // CASE STUDI:LOGIN MIDDLEWARE
-    public function daftarMahasiswa()
+    public function daftarMahasiswa(Request $request)
     {
-        return view('halaman', ['judul' => 'Daftar Mahasiswa']);
+        return view('halaman', ['judul' => 'Daftar Mahasiswa', 'content' => 'Ini halaman Daftar Mahasiswa']);
+
     }
 
     public function tabelMahasiswa()
     {
-        return view('halaman', ['judul' => 'Tabel Mahasiswa']);
+        return view('halaman', ['judul' => 'Tabel Mahasiswa', 'content' => 'Ini halaman Tabel Mahasiswa']);
     }
 
     public function blogMahasiswa()
     {
-        return view('halaman', ['judul' => 'Blog Mahasiswa']);
+        return view('halaman', ['judul' => 'Blog Mahasiswa', 'content' => 'Ini halaman Blog Mahasiswa']);
     }
-
-    public function login()
-    {
-        return view('form-login');
-    }
-
-    public function prosesLogin(Request $request)
-    {
-        $request->validate([
-            'username' => 'required',
-        ]);
-
-        $validUsername = ['andi','rani','lisa','joko'];
-        if(in_array($request->username, $validUsername))
-        {
-            session(['username' => $request->username]);
-            return redirect('/daftar-mahasiswa');
-        }
-        else
-        {
-            return back()->withInput()->with('pesan',"Username tidak valid");
-        }
-    }
-
-    public function logout()
-    {
-        session()->forger('username');
-        return redirect('login')->with('pesan', 'Logout berhasil');
-    }
-
 }
--- web.php
+++ web.php
@@ -2,24 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\JurusanController;
 
 
+Auth::routes();
 
-Route::fallback(function() {
-    return "Maaf alamat tidak ditemukan";
-});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
-// CASE STUDI:LOGIN MIDDLEWARE
-Route::get('/login', [MahasiswaController::class, 'login']);
-Route::post('/login', [MahasiswaController::class, 'prosesLogin']);
-
-Route::get('/logout', [MahasiswaController::class, 'logout']);
-
-Route::redirect('/', '/login');
-
-Route::get('/daftar-mahasiswa', [MahasiswaController::class,'daftarMahasiswa'])->middleware('login');
-
-Route::get('/tabel-mahasiswa', [MahasiswaController::class, 'tabelMahasiswa'])->middleware('login');
-
-Route::get('/blog-mahasiswa', [MahasiswaController::class, 'blogMahasiswa'])->middleware('login');
+Route::get('/daftar-mahasiswa', [MahasiswaController::class, 'daftarMahasiswa'])->middleware('auth');
+Route::get('/tabel-mahasiswa', [MahasiswaController::class, 'tabelMahasiswa'])->middleware('auth');
+Route::get('/blog-mahasiswa', [MahasiswaController::class, 'blogMahasiswa'])->middleware('auth');
+Route::get('/', [JurusanController::class, 'index'])->middleware('auth');
+Route::resource('jurusans',JurusanController::class)->middleware('auth');
+Route::get('/jurusans/{jurusan}', [JurusanController::class, 'show'])
+->name('jurusans.show')->middleware('auth')->middleware('can:view,jurusan');
